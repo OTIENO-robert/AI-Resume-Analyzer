3c9ddf8b32b023ec585d30918821f9ac35fd0308
OTIENO-robert <111677402+OTIENO-robert@users.noreply.github.com>
2025-03-26 05:36:03 +0300

    Add files via upload

---
+++ AIRES-SYSTEM/src/components/ui/progress.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './jsxc.css';
+
+export const Progress = ({ value, className = '' }) => (
+  <div className={`progress-container ${className}`}>
+    <div
+      style={{ width: `${value}%` }}
+      className="progress-fill"
+    ></div>
+  </div>
+);
